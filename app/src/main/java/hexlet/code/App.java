1b8702dfe7f95f02f7018de311ef49d58c1232fa
Даниил <rylov2099@gmail.com>
2025-03-08 22:05:11 +0300

    app start

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,24 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.start();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        Scanner scanner = new Scanner(System.in);
+        var gameNumber = scanner.nextInt();
+        if (gameNumber == 1) {
+            System.out.println("Your choice: 1");
+            System.out.println("Welcome to the Brain Games!");
+            Cli.start();
+        } else if (gameNumber == 2) {
+            System.out.println("Your choice: 2");
+            System.out.println("Welcome to the Brain Games!");
+            Parity.parityCheck();
+        }
+
     }
 }
