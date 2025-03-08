146bb4ce9de7adb219a56f81c2a230e7a2c21078
Даниил <rylov2099@gmail.com>
2025-03-09 01:12:00 +0300

    game menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,6 +7,7 @@ public class App {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
         Scanner scanner = new Scanner(System.in);
         var gameNumber = scanner.nextInt();
@@ -18,7 +19,10 @@ public class App {
             System.out.println("Your choice: 2");
             System.out.println("Welcome to the Brain Games!");
             Parity.parityCheck();
+        } else if (gameNumber == 3) {
+            System.out.println("Your choice: 3");
+            System.out.println("Welcome to the Brain Games!");
+            Calc.calc();
         }
-
     }
 }
