fdd940fa22d65450e3035b7b017fcd053799e140
Даниил <rylov2099@gmail.com>
2025-03-09 16:09:52 +0300

    prime game

--- app/src/main/java/hexlet/code/Prime.java
+++ app/src/main/java/hexlet/code/Prime.java
@@ -6,7 +6,9 @@ import java.util.Scanner;
 public class Prime {
 
     private static String isPrime(int n) {
-        if (n < 2) return "no";
+        if (n < 2) {
+            return "no";
+        }
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) return "no";
         } return "yes";
@@ -18,14 +20,16 @@ public class Prime {
         Scanner scanner = new Scanner(System.in);
         var userName = scanner.next();
 
-        System.out.println("Hello, " + userName+ "!");
+        System.out.println("Hello, " + userName + "!");
         System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
 
+        var gameWin = 3;
+        var bound = 50;
         var correctAnswers = 0;
-        while (correctAnswers < 3) {
+        while (correctAnswers < gameWin) {
 
             Random random = new Random();
-            int num = random.nextInt(50);
+            int num = random.nextInt(bound);
             var correctAnswer = isPrime(num);
 
             System.out.println("Question: " + num);
