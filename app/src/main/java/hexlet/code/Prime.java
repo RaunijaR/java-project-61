51e53c93ca642a176e2b132ff87f1f9dca194093
Даниил <rylov2099@gmail.com>
2025-03-09 03:32:04 +0300

    prime game

---
+++ app/src/main/java/hexlet/code/Prime.java
@@ -0,0 +1,48 @@
+package hexlet.code;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Prime {
+    private static String isPrime(int N) {
+        if (N < 2) return "no";
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if (N % i == 0) return "no";
+        }
+        return "yes";
+    }
+
+    public static void prime() {
+        System.out.println("May I have your name? ");
+
+        Scanner scanner = new Scanner(System.in);
+        var userName = scanner.next();
+
+        System.out.println("Hello, " + userName + "!");
+
+        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+
+        var correctAnswers = 0;
+        while (correctAnswers < 3) {
+            Random random = new Random();
+            int num = random.nextInt(3000);
+            var correctAnswer = isPrime(num);
+
+            System.out.println("Question: " + num);
+            System.out.println("Your answer: ");
+
+            Scanner scan = new Scanner(System.in);
+            var userAnswer = scan.next();
+
+            if (userAnswer.equals(correctAnswer)) {
+                System.out.println("Correct!");
+                correctAnswers++;
+            } else {
+                System.out.println(userAnswer + " - wrong answer ;(. Correct answer was - " + correctAnswer);
+                System.out.println("Let's try again, " + userName + "!");
+                return;
+            }
+        }
+        System.out.println("Congratulations, " + userName + "!");
+        }
+    }
